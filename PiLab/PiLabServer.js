3b8933674278c3c1bdf1970fee8f8795760b8322
bkomac <b@komac.si>
2015-01-11 19:49:12 +0100

    vv

--- PiLab/PiLabServer.js
+++ PiLab/PiLabServer.js
@@ -15,7 +15,8 @@ app.get('/', function(req, res) {
 	res.end('This is socket.io endpoint on port ' + port + ' \n');
 });
 
-app.use(express.static(__dirname + '/app/*'));
+app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/app/bower_components'));
 
 io.on('connection', function(socket) {
 	var address = socket.handshake.address;

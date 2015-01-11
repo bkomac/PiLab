c33ce6bede64dff4d4741fce990be3715efd2774
bkomac <b@komac.si>
2015-01-11 19:44:56 +0100

    cc

--- PiLab/PiLabServer.js
+++ PiLab/PiLabServer.js
@@ -5,11 +5,15 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var sessions = [];
-var numOnlineUsers;
-
 console.log('** Starting PiLabServer node on port ' + port + '...');
 
+app.get('/', function(req, res) {
+	res.writeHead(200, {
+		'Content-Type' : 'text/plain'
+	});
+	console.log("GET request...");
+	res.end('This is socket.io endpoint on port ' + port + ' \n');
+});
 
 app.use(express.static(__dirname + '/app'));
 
@@ -18,15 +22,11 @@ io.on('connection', function(socket) {
 	console.log("*** Conecting ... #" + socket.id + " " + socket.request.connection.remoteAddress);
 
 	socket.on('put_position', function(msg) {
-		
-		
+
 	});
-	
 
 });
 
 http.listen(port, function() {
 	console.log('** PiLabServer listening on:' + port + ' ...');
 });
-
-

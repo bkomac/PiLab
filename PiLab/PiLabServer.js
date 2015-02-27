b83a963768dc037f151005f3b0cfbfdcb0ffb521
bkomac <b@komac.si>
2015-02-27 20:00:42 +0100

    tt

--- PiLab/PiLabServer.js
+++ PiLab/PiLabServer.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var url = require('url');
 
 var piblaster = require('pi-blaster.js');
 
@@ -12,19 +13,29 @@ var leftMotorRwdGpio = 17;
 var rightMotorFwdGpio = 18;
 var rightMotorRwsGpio = 21;
 
-console.log('** Starting PiLabServer node on port ' + port + '...');
+console.log('*** Starting PiLabServer node on port ' + port + '...');
+
+// app.get('/*', function(req, res) {
+// res.writeHead(200, {
+// 'Content-Type' : 'text/plain'
+// });
+//
+// var drive = url.parse(req.url).pathname.slice(1);
+// console.log("GET request...turn:" + drive);
+// if (drive != 0) {
+// moveCar({
+// "turn" : drive
+// });
+// } else
+// moveCar({
+// "turn" : 0
+// });
+// res.end('Turn= ' + drive + ' \n');
+// });
 
 app.use(express.static(__dirname + '/app'));
 // app.use('/bower_components', express.static(__dirname +
 // '/bower_components'));
-
-// app.get('/', function(req, res) {
-// res.writeHead(200, {
-// 'Content-Type' : 'text/plain'
-// });
-// console.log("GET request...");
-// res.end('This is socket.io endpoint on port ' + port + ' \n');
-// });
 
 io.on('connection', function(socket) {
 	var address = socket.handshake.address;
@@ -47,26 +58,45 @@ http.listen(port, function() {
 
 function moveCar(msg) {
 
-	if (msg.turn < -2) {
+	msg.speed = msg.speed * 10;
+
+	// turn right
+	if (msg.turn < -20) {
+		piblaster.setPwm(rightMotorFwdGpio, calibrate(msg.speed));
+
 		piblaster.setPwm(leftMotorFwdGpio, calibrate(msg.turn));
 		piblaster.setPwm(leftMotorRwdGpio, 0);
-	} else if (msg.turn > 2) {
+		console.log('Turning right ...' + calibrate(msg.turn) + ' speed ' + calibrate(msg.speed));
+
+		// turn left
+	} else if (msg.turn > 20) {
+		piblaster.setPwm(leftMotorFwdGpio, calibrate(msg.speed));
+
 		piblaster.setPwm(rightMotorFwdGpio, calibrate(msg.turn));
 		piblaster.setPwm(rightMotorRwsGpio, 0);
+		console.log('Turning left ...' + calibrate(msg.turn) + ' speed ' + calibrate(msg.speed));
 	} else {
-		piblaster.setPwm(leftMotorFwdGpio, 0);
+		piblaster.setPwm(leftMotorFwdGpio, calibrate(msg.speed));
 		piblaster.setPwm(leftMotorRwdGpio, 0);
-		piblaster.setPwm(rightMotorFwdGpio, 0);
+		piblaster.setPwm(rightMotorFwdGpio, calibrate(msg.speed));
 		piblaster.setPwm(rightMotorRwsGpio, 0);
+		console.log('Stopping ...' + calibrate(msg.turn) + ' speed ' + calibrate(msg.speed));
 	}
 
 }
 
 function calibrate(turn) {
 
-	var out = turn * 10 + 30;
+	if (turn == 0)
+		return 0;
+
+	if (turn < 0)
+		turn = turn * -1;
+
+	var out = turn + 30;
+
 	if (out > 100)
 		out = 100;
 
-	return out;
+	return out / 100;
 }

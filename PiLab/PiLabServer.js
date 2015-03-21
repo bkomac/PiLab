4df2aec21d51dcccb79a84422873ac03f2b837c9
bkomac <b@komac.si>
2015-03-21 18:22:41 +0100

    cc

--- PiLab/PiLabServer.js
+++ PiLab/PiLabServer.js
@@ -160,17 +160,27 @@ function calibrate(turn) {
 };
 
 function exitHandler(options, err) {
-    if (options.cleanup) console.log('clean');
-    if (err) console.log(err.stack);
-    if (options.exit) process.exit();
+	if (options.cleanup) {
+		stop(msg);
+		console.log('** PiLabServer closing down...');
+	}
+	if (err)
+		console.log(err.stack);
+	if (options.exit)
+		process.exit();
 }
 
-//do something when app is closing
-process.on('exit', exitHandler.bind(null,{cleanup:true}));
+// do something when app is closing
+process.on('exit', exitHandler.bind(null, {
+	cleanup : true
+}));
 
-//catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+// catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {
+	exit : true
+}));
 
-//catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
-
+// catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {
+	exit : true
+}));

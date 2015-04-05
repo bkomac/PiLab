6fee240011fe0872b43cd91861b34c66b417408d
bkomac <b@komac.si>
2015-04-05 18:48:01 +0200

    cc

--- PiLab/PiLabServer.js
+++ PiLab/PiLabServer.js
@@ -63,7 +63,7 @@ app.use(express.static(__dirname + '/app'));
 io.on('connection', function(socket) {
 	var address = socket.handshake.address;
 	console.log("*** Conecting ... #" + socket.id + " " + socket.request.connection.remoteAddress);
-	console.log("***"+socket.handshake.address);
+	console.log("***"+JSON.stringify(socket.handshake));
 	socket.emit('connected', {
 		socketId : socket.id,
 		clientIp : socket.request.connection.remoteAddress,
